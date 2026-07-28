7bb21286458af5ff7ab980dae79834d28e99e74c
kayla-lna <kaylaluna2806@gmail.com>
2026-07-28 10:27:25 +0700

    push

--- JsDassar-WebSekolah-Kayla/app.js
+++ JsDassar-WebSekolah-Kayla/app.js
@@ -1,3 +1,5 @@
-import { KontakPage } from "./pages/KontakPage.js";
-const page = new KontakPage();
-document.getElementById("app").innerHTML = page.render();
+import { router } from "./router.js";
+// Jalankan router setiap kali hash di URL berubah (user klik menu)
+window.addEventListener("hashchange", router);
+// Jalankan router pertama kali saat halaman pertama dibuka
+window.addEventListener("DOMContentLoaded", router);
