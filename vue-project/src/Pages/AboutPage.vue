c5e20d9f2810640d3918df888a04dea693fa571e
kayla-lna <kaylaluna2806@gmail.com>
2026-08-04 09:04:15 +0700

    kayla imut

---
+++ vue-project/src/Pages/AboutPage.vue
@@ -0,0 +1,7 @@
+<template>
+ <main>
+ <h1>Tentang Kami</h1>
+ <p>Onlineshop ini dikelola oleh siswa SMK Ceriya jurusan
+PPLG.</p>
+ </main>
+</template>
---
+++ vue-project/src/Pages/LandingPage.vue
@@ -0,0 +1,6 @@
+<template>
+ <main>
+ <h1>Selamat Datang di Onlineshop </h1>
+ <p>Belanja gampang, mudah, dan senang.</p>
+ </main>
+</template>
